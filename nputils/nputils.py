a1affc86738091ad6971d3235b6ff103a8f74a70
Yannic Kilcher <yannic.kilcher@gmail.com>
2015-11-18 18:04:37 +0100

    log plot

--- nputils/nputils.py
+++ nputils/nputils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def simplex_volume(simplex_points):
@@ -24,3 +25,27 @@ def msqrt(M):
     (u, s, vt) = np.linalg.svd(M)
     ms = np.dot(u, np.dot(np.diag(np.sqrt(s)), vt))
     return ms
+
+
+def score_log_plot(results, title, subtract_min=True):
+    """
+    results must be a list of tuples
+    (label, xs, means, stds)
+    """
+    minScore = np.min([r[2] for r in results]) + 1e-13 if subtract_min else 0.0
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for result in results:
+        meanScores = result[2]
+        meanScores -= minScore
+        stdScores = result[3]
+        epochs = result[1]
+        base_line, = ax.plot(epochs, meanScores, label=str(result[0]))
+        ax.fill_between(epochs, meanScores - stdScores, meanScores + stdScores, alpha=0.5,
+                        facecolor=base_line.get_color())
+    ax.set_yscale('log')
+    ax.legend(loc=0)
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Objective Value")
+    ax.set_title("title")
+
