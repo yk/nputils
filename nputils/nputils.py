5c232cf74b86751dd54727290b40f7870be8e359
Yannic Kilcher <yannic.kilcher@gmail.com>
2015-12-11 15:39:33 +0100

    xscale

--- nputils/nputils.py
+++ nputils/nputils.py
@@ -32,7 +32,7 @@ def normalizev(v, norm='l2'):
     return np.ravel(sklearn.preprocessing.normalize(v[np.newaxis, :], norm=norm))
 
 
-def score_log_plot(results, title, subtract_min=True, ylabel="Objective Value"):
+def score_log_plot(results, title, subtract_min=True, ylabel="Objective Value", xscale="linear"):
     """
     results must be a list of tuples
     (label, epochs, means, stds)
@@ -48,6 +48,7 @@ def score_log_plot(results, title, subtract_min=True, ylabel="Objective Value"):
         base_line, = ax.plot(epochs, subMeanScores, label=str(result[0]))
         ax.fill_between(epochs, subMeanScores - stdScores, subMeanScores + stdScores, alpha=0.5,
                         facecolor=base_line.get_color())
+    ax.set_xscale(xscale)
     ax.set_yscale('log')
     ax.legend(loc=0)
     ax.set_xlabel("Epoch")
